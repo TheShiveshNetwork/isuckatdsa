77ec6204d84573f6a2b875e1787bec3ea30faf5a
Shivesh Tiwari <shiveshtiwari0@gmail.com>
2026-09-08 07:12:51 +0530

    feat: google analytics integration

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -29,6 +29,7 @@ export const SITE = {
     "i suck at dsa - a wiki of DSA mental models and patterns for people who suck at dsa.",
   favicon: "/favicon.svg",
   googleSiteVerification: "S5gLXznDgd9e2bHFaus04MerHleWClbDTEaqSSLtV4s",
+  googleAnalyticsId: "G-MY6DSDEV6Z",
   twitterHandle: "@isuckatdsa",
   githubHandle: "isuckatdsa",
   social: {
